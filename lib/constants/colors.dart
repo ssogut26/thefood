cf5593801286e8dbc7308a01ee108edc8a4d9daa
Selcuk <ssogut26@gmail.com>
2022-09-29 22:26:28 +0300

    add colors and paddings

---
+++ lib/constants/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/painting.dart';
+
+class ProjectColors {
+  static const ocher = Color(0xFFDB8213);
+  static const white = Color(0xFFFFFFFF);
+  static const black = Color(0xFF000000);
+  static const black50 = Color.fromRGBO(0, 0, 0, 0.5);
+  static const black25 = Color.fromRGBO(0, 0, 0, 0.25);
+  static const softGreen = Color(0xFF6FCC66);
+}
---
+++ lib/constants/paddings.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/widgets.dart';
+
+class ProjectPaddings {
+  EdgeInsets get small => const EdgeInsets.all(4);
+  EdgeInsets get medium => const EdgeInsets.all(8);
+  EdgeInsets get large => const EdgeInsets.all(12);
+}
